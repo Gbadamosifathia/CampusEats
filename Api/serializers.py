5d9e864549f7e18235c7b70b70c94352b03f9f5d
violetmoorway-9942 <violetmoorway@gmail.com>
2026-08-27 17:50:03 +0100

    Fix order creation failing due to missing user in serializer

--- Api/serializers.py
+++ Api/serializers.py
@@ -16,6 +16,7 @@ class OrderSerializers(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+        read_only_fields = ['user']
 
 class OrderItemSerializers(serializers.ModelSerializer):
     class Meta:
